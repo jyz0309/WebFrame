bd3f5da46da30b43b725a2823388354bec6ce484
jyz0309 <38248129+jyz0309@users.noreply.github.com>
2019-08-22 22:07:44 +0800

    Test The Change

--- test.py
+++ test.py
@@ -1,23 +1,27 @@
-
-from app import App,View
+from app import App,View,session
 import json
+
+
 class Index(View):
-    def GET(self,request):
-        return "hello world"
+    def GET(self,request,x):
+        #session['hello'] = 2
+        return x
     def POST(self,request):
-        # print(json.dumps(request.form['color']))
+        print(json.dumps(request.form['color']))
         return json.dumps({'1':'hello'})
 
 class Test(View):
     def GET(self,request):
-        return "test"
-    def DELETE(self,request):
+        # print(session['hello1'])
+        return 'test'
+    def POST(self,request):
         return json.dumps({'2':'hello'})
-urls = {'/':Index,
+
+
+urls = {'/<x>':Index, #动态路由
         '/test':Test}
 
-
-
 app = App()
+app.secret_key = 'password'
 app.add_url_rule(urls)
 app.run()
